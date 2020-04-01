1dfa9a00078d7b249b07658b22fa876ae1adc889
Jim Wardlaw <jim.wardlaw@gmail.com>
2020-04-02 00:52:02 +0100

    Feat (docs) make the iframe width resizable

--- app/components/component-demo.js
+++ app/components/component-demo.js
@@ -1,18 +1,35 @@
 import Component from '@ember/component';
 import {
+  computed
+} from '@ember/object';
+import {
   equal
 } from '@ember/object/computed';
 import {
   getCodeSnippet
 } from 'ember-code-snippet';
+import {
+  inject as service
+} from '@ember/service';
+
+const dragDirections = ["right"];
 
 export default Component.extend({
+  // Service
+  router: service(),
   // Properties
   tab: 'preview',
   tagName: '',
+  width: 320,
+  minWidth: 320,
+  maxWidth: 1200,
+  directions: dragDirections,
   // Computed
   showPreview: equal('tab', 'preview'),
   showCode: equal('tab', 'code'),
+  url: computed('src', function() {
+    return this.router.urlFor(this.src);
+  }),
   // Actions
   actions: {
     setTab(tab) {
@@ -28,6 +45,15 @@ export default Component.extend({
         .catch(() => {
           alert('Failed to copy text.');
         });
-    }
+    },
+    resizeStart(direction, event, element) {
+      // if the iframe is accepting mouse events when we are rezising
+      // it will swallow up our mouse event and cause the drag to never end
+      element.querySelector('.demo__frame').style.pointerEvents = "none";
+    },
+    resizeStop(direction, event, element) {
+      // allow default iframe interaction after the drag ends
+      element.querySelector('.demo__frame').style.pointerEvents = "auto";
+    },
   }
 });
