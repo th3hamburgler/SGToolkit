e9f180f218c3ca5e07ec1df7e1acee90eca2ef79
Jim Wardlaw <jim.wardlaw@gmail.com>
2020-04-02 11:45:31 +0100

    Feat (docs) resize demo iframe height to fit content

--- app/components/component-demo.js
+++ app/components/component-demo.js
@@ -35,7 +35,10 @@ export default Component.extend({
   didRender() {
     this._super(...arguments);
     // set max with to width of the component
-    this.set('maxWidth', this.element.clientWidth);
+    const maxWidth = this.element.clientWidth,
+      defaultWidth = (maxWidth / 1.5) > this.minWidth ? (maxWidth / 1.5) : this.minWidth;
+    this.set('maxWidth', maxWidth);
+    this.set('width', defaultWidth);
   },
   // Actions
   actions: {
@@ -62,5 +65,13 @@ export default Component.extend({
       // allow default iframe interaction after the drag ends
       element.querySelector('.demo__frame').style.pointerEvents = "auto";
     },
+    setPreviewHeight( /*event*/ ) {
+      const iframe = this.element.querySelector('.demo__frame'),
+        height = iframe.contentWindow.document.documentElement.scrollHeight
+
+      if (height) {
+        iframe.style.height = height + 'px';
+      }
+    }
   }
 });
